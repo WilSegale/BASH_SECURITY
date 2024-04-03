4bb6abb9139ed981a837075480e92f426f8ec64b
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-04-03 18:30:08 -0400

    auto update

--- secuirty.sh
+++ secuirty.sh
@@ -1,24 +1,63 @@
 #!/bin/bash
-
-
 source DontEdit.sh
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Function to handle cleanup on exit
+# quits program with ctrl-c
+EXIT_PROGRAM_WITH_CTRL_C() {
+    echo -e "${RED}[-]${NC} EXITING SOFTWARE..."
+    
+    # Adds a cleanup commands here
+    exit 1
+}
+
+# quits program with ctrl-z
+EXIT_PROGRAM_WITH_CTRL_Z(){
+    echo ""
+    echo -e "${RED}[-]${NC} EXITING SOFTWARE..."
+    # Add cleanup commands here
+    exit 1
+}
+
+# Function to be executed when Ctrl+Z is pressed
+handle_ctrl_z() {
+    EXIT_PROGRAM_WITH_CTRL_Z
+    exit 1
+    # Your custom action goes here
+}
+
+# Set up the trap to call the function on SIGTSTP (Ctrl+Z)
+trap 'handle_ctrl_z' SIGTSTP
+
+# Function to handle Ctrl+C
+ctrl_c() {
+    echo ""
+    EXIT_PROGRAM_WITH_CTRL_C
+}
+
+trap ctrl_c SIGINT
 
 function scan(){
 
-    read -p "Auto mode or Manule: " autoOrMan
+    read -p "Auto/Manule: " autoOrMan
     # gets a argument in a auto array
     if [[ " ${autoOrMan[*]} " == *" ${auto} "* ]]; then
-        echo "Auto mode"
+        echo -e "[ ${GREEN}OK${NC} ] Auto mode"
         nmap --script=malware -Pn 192.168.1.1/24
 
     # gets a argument in a manule array
     elif [[ " ${autoOrMan[*]} " == *" ${manule} "* ]]; then
-        echo "Manule mode."
+        echo -e "[ ${GREEN}OK${NC} ] Manule mode"
         read -p "Enter the ip: " ip
         read -p "Enter the port: " port
-        nmap --script=malware "${port}" -p "${ip}"
+        nmap --script=malware "${ip}" -p "${port}" 
+    #eror message
     else
-        echo "ERROR"
+        echo -e "[ ${RED}FAIL${NC} ] Wrong argument"
         scan
     fi
 }
